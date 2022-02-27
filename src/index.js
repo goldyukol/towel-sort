50762671ffbbead23929fdf50ec166fe5eeff4ca
Yury Kalenkou <goldyukol12@gmail.com>
2022-02-27 15:52:13 +0300

    feat: change to cycles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 module.exports = function towelSort(matrix) {
-  if(!matrix) return [];
+    if (!matrix) return [];
+
+    const res = [];
 
     for (let i = 0; i < matrix.length; i++) {
-        if (i % 2 !== 0) {
-            matrix[i].reverse();
+        for (let j = 0; j < matrix[i].length; j++) {
+            let index = i % 2 === 0 ? j : matrix[i].length - 1 - j;
+
+            res.push(matrix[i][index]);
         }
     }
 
-    return matrix.flat();
+    return res;
 };
